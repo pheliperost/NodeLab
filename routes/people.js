5168f31b69a5cef8aeab747eea49bb77d6997a39
pheliperost <phelipe_rost@hotmail.com>
2020-04-14 13:28:24 -0300

    update people

--- routes/people.js
+++ routes/people.js
@@ -14,18 +14,32 @@ Router.get("/", (req, res)=>{
 
 })
 
-/*
-app.post('/learners', (req, res) => {
-    mysqlconnection.query("insert into people values ('tiburcio,69')", (err,rows,fields)=>{
-        if(!err){
-            res.send(rows);
-        }else{
-            console.log(err);
-        }
-
+//Router to GET specific learner detail from the MySQL database
+Router.get('/:id' , (req, res) => {
+    mysqlconnection.query('SELECT * FROM people WHERE age = ?',[req.params.id], (err, rows, fields) => {
+    if (!err)
+    res.send(rows);
+    else
+    console.log(err);
     })
+    } );
 
+
+Router.post('/Insert', (req, res) => {
+    let people = req.body;
+    var sql = "SET @name = ?; SET @age = ?; call peopleAdd(@name,@age);";
+
+//CALL learnerAddOrEdit(@name,@age);";
+    mysqlconnection.query(sql, [people.name, people.age], (err, rows, fields) => {
+        if (!err)
+        rows.forEach(element => {
+        if(element.constructor == Array)
+        res.send('New People Added : '+ element[0].name);
+        });
+        else
+        console.log(err);
+        })
 });
-*/
+
 module.exports = Router;
 
